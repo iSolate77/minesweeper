cd38d9349bb490ffa8188b245d64862444ae183e
iSolate77 <mohd.faris07@gmail.com>
2022-10-06 11:40:07 +0300

    practice

---
+++ thing.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  std::string x = "";
+  std::cout << "Type something: " << std::endl;
+  std::cin >> x;
+  std::cout << "This is what you typed: " <<  x << std::endl;
+
+  return 0;
+}
